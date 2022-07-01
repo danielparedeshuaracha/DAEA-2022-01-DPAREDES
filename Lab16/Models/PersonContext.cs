42b172be4d261389e659fafeb5d5491b1f4f7893
danielparedeshuaracha <danielparedeshuaracha@gmail.com>
2022-07-01 01:07:33 -0500

    lab 16 finished

---
+++ Lab16/Models/PersonContext.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab16.Models
+{
+    public class PersonContext: DbContext
+    {
+        public PersonContext(DbContextOptions<PersonContext> options)
+    : base(options)
+        {
+
+        }
+        public DbSet<Person> Person { get; set; }
+
+    }
+}
